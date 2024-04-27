974e1b67f86b1074f7f147e01260881b3a9ef518
Miguel Cañate <94227693+MiguelCagnate@users.noreply.github.com>
2024-04-27 22:30:47 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="mt-4 p-20 text-7xl font-extrabold text-center text-gray-900 dark:text-white md:text-5xl lg:text-9xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Milo's Template</span></h1>
+ 
+    </>
+  )
+}
+
+export default App
